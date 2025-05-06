ed6f9885f2b12c94d4c47da7689040f172467b77
aadam_oc <Adam Ortiz@DESKTOP-UBNO7O5>
2025-05-06 18:12:26 +0200

    1

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+// /lib/firebase.ts
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+// Solo inicializar si no está ya inicializado
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
